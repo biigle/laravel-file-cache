3172add7ec1cc1d79062ebc0fd7d28e2f822f98d
Martin Zurowietz <martin@zurowietz.de>
2024-07-12 16:35:02 +0200

    Implement compatibility with Laravel 11
    Resolves https://github.com/biigle/laravel-file-cache/issues/22

--- tests/FileCacheServiceProviderTest.php
+++ tests/FileCacheServiceProviderTest.php
@@ -12,7 +12,7 @@ class FileCacheServiceProviderTest extends TestCase
     {
         config(['file-cache.prune_interval' => '*/5 * * * *']);
         $schedule = $this->app[Schedule::class];
-        $event = $schedule->events()[0];
+        $event = $schedule->events()[1];
         $this->assertStringContainsString('prune-file-cache', $event->command);
         $this->assertEquals('*/5 * * * *', $event->expression);
     }
